5c0fcfa9ca4ea286d5de00d8195b7f87b7f075d6
Viet Anh Tong <vietanhtong1508@gmail.com>
2021-02-09 13:22:28 +0700

    init project

---
+++ src/test/java/com/vag/gamecenter/GameCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vag.gamecenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
