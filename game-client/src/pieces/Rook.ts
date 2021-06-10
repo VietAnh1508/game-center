3f76160b01a7c0c39f6dea0ef47b0db57dd6935d
Tong Thien Viet Anh <anh.tong@elca.vn>
2021-06-10 21:22:25 +0700

    add Rook's move

--- game-client/src/pieces/Rook.ts
+++ game-client/src/pieces/Rook.ts
@@ -7,10 +7,34 @@ export default class Rook extends Piece {
     }
 
     isMovePossible(src: Coordinate, dest: Coordinate): boolean {
-        return true;
+        return src.x === dest.x || src.y === dest.y;
     }
 
     getSrcToDestPath(src: Coordinate, dest: Coordinate): Array<Coordinate> {
-        return [];
+        let path: Array<Coordinate> = [];
+
+        if (src.x === dest.x) {
+            let startRow = Math.min(src.y, dest.y),
+                endRow = Math.max(src.y, dest.y);
+
+            for (let row = startRow + 1; row < endRow; row++) {
+                path.push({
+                    x: src.x,
+                    y: row
+                });
+            }
+        } else if (src.y === dest.y) {
+            let startCol = Math.min(src.x, dest.x),
+                endCol = Math.max(src.x, dest.x);
+
+            for (let col = startCol + 1; col < endCol; col++) {
+                path.push({
+                    x: col,
+                    y: src.y
+                });
+            }
+        }
+
+        return path;
     }
 }
