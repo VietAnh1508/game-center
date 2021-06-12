98bb6460d7682fa1fb8b1f6618805b3e4008584a
Tong Thien Viet Anh <anh.tong@elca.vn>
2021-06-12 22:51:18 +0700

    implement get possible moves for bishop

--- game-client/src/pieces/Bishop.ts
+++ game-client/src/pieces/Bishop.ts
@@ -15,7 +15,64 @@ export default class Bishop extends Piece {
         return [...getPointsBetween2PointsOnDiagonal(src, dest)];
     }
 
-    getPossibleMoves(curPos: Coordinate): Array<Coordinate> {
-        return [];
+    getPossibleMoves(
+        curPos: Coordinate,
+        squares: Array<Array<Piece | null>>
+    ): Array<Coordinate> {
+        let moves = [];
+
+        // top left
+        let x = curPos.x - 1,
+            y = curPos.y + 1;
+        while (x >= 0 && y < 8 && squares[y][x]?.color !== this.color) {
+            moves.push({
+                x,
+                y
+            });
+
+            x--;
+            y++;
+        }
+
+        // top right
+        x = curPos.x + 1;
+        y = curPos.y + 1;
+        while (x < 8 && y < 8 && squares[y][x]?.color !== this.color) {
+            moves.push({
+                x,
+                y
+            });
+
+            x++;
+            y++;
+        }
+
+        // bottom left
+        x = curPos.x - 1;
+        y = curPos.y - 1;
+        while (x >= 0 && y >= 0 && squares[y][x]?.color !== this.color) {
+            moves.push({
+                x,
+                y
+            });
+
+            x--;
+            y--;
+        }
+
+        // bottom right
+        x = curPos.x + 1;
+        y = curPos.y - 1;
+        while (x < 8 && y >= 0 && squares[y][x]?.color !== this.color) {
+            moves.push({
+                x,
+                y
+            });
+
+            x++;
+            y--;
+        }
+
+        return moves;
     }
 }
