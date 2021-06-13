ca58fb8bbf5f04ab7088ed28c73f89d80eafdca1
Tong Thien Viet Anh <anh.tong@elca.vn>
2021-06-13 10:22:59 +0700

    get possible moves for rook

--- game-client/src/pieces/Rook.ts
+++ game-client/src/pieces/Rook.ts
@@ -15,7 +15,63 @@ export default class Rook extends Piece {
         return [...getPointsBetween2PointsOnStraightLine(src, dest)];
     }
 
-    getPossibleMoves(curPos: Coordinate): Array<Coordinate> {
-        return [];
+    getPossibleMoves(
+        curPos: Coordinate,
+        squares: Array<Array<Piece | null>>
+    ): Array<Coordinate> {
+        let moves = [];
+
+        let x = curPos.x,
+            y = curPos.y + 1;
+
+        // top
+        while (y < 8 && squares[y][x]?.color !== this.color) {
+            moves.push({ x, y });
+
+            if (this.meetFirstOpponent(squares[y][x])) {
+                break;
+            }
+
+            y++;
+        }
+
+        // down
+        y = curPos.y - 1;
+        while (y >= 0 && squares[y][x]?.color !== this.color) {
+            moves.push({ x, y });
+
+            if (this.meetFirstOpponent(squares[y][x])) {
+                break;
+            }
+
+            y--;
+        }
+
+        // left
+        x = curPos.x - 1;
+        y = curPos.y;
+        while (x >= 0 && squares[y][x]?.color !== this.color) {
+            moves.push({ x, y });
+
+            if (this.meetFirstOpponent(squares[y][x])) {
+                break;
+            }
+
+            x--;
+        }
+
+        // right
+        x = curPos.x + 1;
+        while (x < 8 && squares[y][x]?.color !== this.color) {
+            moves.push({ x, y });
+
+            if (this.meetFirstOpponent(squares[y][x])) {
+                break;
+            }
+
+            x++;
+        }
+
+        return moves;
     }
 }
